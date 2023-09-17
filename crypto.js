c5aab4314967842056aaf6710c3a4bccc8f148a5
Adam Bronte <adam@corgea.com>
2023-09-17 11:34:58 -0700

    code cleanup

--- crypto.js
+++ crypto.js
@@ -10,29 +10,29 @@ async function generateKeyPair() {
     // Generate an RSA key pair for encryption
     const keyPair = await window.crypto.subtle.generateKey(
         {
-            name: "RSA-OAEP",
+            name: 'RSA-OAEP',
             modulusLength: 2048, // Key size
             publicExponent: new Uint8Array([1, 0, 1]), // Public exponent
-            hash: "SHA-256", // Hash algorithm
+            hash: 'SHA-256', // Hash algorithm
         },
         true, // Extractable
-        ["encrypt", "decrypt"] // Key usages
+        ['encrypt', 'decrypt'] // Key usages
     );
 
     // Export the public key to JWK format
     const publicKeyJWK = await window.crypto.subtle.exportKey(
-        "jwk",
+        'jwk',
         keyPair.publicKey
     );
 
     // Export the private key to JWK format
     const privateKeyJWK = await window.crypto.subtle.exportKey(
-        "jwk",
+        'jwk',
         keyPair.privateKey
     );
 
-    pub_key_ser = serialize_key(publicKeyJWK);
-    priv_key_ser = serialize_key(privateKeyJWK);
+    let pub_key_ser = serialize_key(publicKeyJWK);
+    let priv_key_ser = serialize_key(privateKeyJWK);
 
     localStorage.setItem(pub_key_ser, priv_key_ser)
 
@@ -41,11 +41,11 @@ async function generateKeyPair() {
 
 async function importJWKey(key, usage) {
     return await window.crypto.subtle.importKey(
-        "jwk", // Format
+        'jwk', // Format
         key, // Key data
         {
-            name: "RSA-OAEP",
-            hash: "SHA-256",
+            name: 'RSA-OAEP',
+            hash: 'SHA-256',
         },
         true, // Extractable
         [usage] // Key usages
@@ -53,8 +53,7 @@ async function importJWKey(key, usage) {
 }
 
 async function load_public_key() {
-   // return location.hash.split("#")[1]
-   return await importJWKey(deserialize_key(location.hash.split("#")[1]), "encrypt")
+    return await importJWKey(deserialize_key(location.hash.split('#')[1]), 'encrypt')
 }
 
 async function load_key(serialized_key, usage) {
@@ -62,43 +61,39 @@ async function load_key(serialized_key, usage) {
 }
 
 async function load_private_key(public_key_ser) {
-    return await importJWKey(deserialize_key(localStorage.getItem(pub_key_ser)),"decrypt")
+    return await importJWKey(deserialize_key(localStorage.getItem(pub_key_ser)), 'decrypt')
 }
 
 async function encryptString(publicKey, plaintext) {
-  // Convert the plaintext string to a buffer
-  const textBuffer = new TextEncoder().encode(plaintext);
+    // Convert the plaintext string to a buffer
+    const textBuffer = new TextEncoder().encode(plaintext);
 
-  // Encrypt the buffer using the public key
-  const encryptedBuffer = await window.crypto.subtle.encrypt(
-    {
-      name: "RSA-OAEP",
-    },
-    publicKey,
-    textBuffer
-  );
+    // Encrypt the buffer using the public key
+    const encryptedBuffer = await window.crypto.subtle.encrypt(
+        {
+            name: 'RSA-OAEP',
+        },
+        publicKey,
+        textBuffer
+    );
 
-  // Convert the encrypted buffer to a base64 string
-  const encryptedBase64 = btoa(String.fromCharCode(...new Uint8Array(encryptedBuffer)));
-
-  return encryptedBase64;
+    // Convert the encrypted buffer to a base64 string
+    return btoa(String.fromCharCode(...new Uint8Array(encryptedBuffer)));
 }
 
 async function decryptString(privateKey, encryptedBase64) {
-  // Convert the base64 string to a buffer
-  const encryptedBuffer = Uint8Array.from(atob(encryptedBase64), c => c.charCodeAt(0));
+    // Convert the base64 string to a buffer
+    const encryptedBuffer = Uint8Array.from(atob(encryptedBase64), c => c.charCodeAt(0));
 
-  // Decrypt the buffer using the private key
-  const decryptedBuffer = await window.crypto.subtle.decrypt(
-    {
-      name: "RSA-OAEP",
-    },
-    privateKey,
-    encryptedBuffer
-  );
+    // Decrypt the buffer using the private key
+    const decryptedBuffer = await window.crypto.subtle.decrypt(
+        {
+            name: 'RSA-OAEP',
+        },
+        privateKey,
+        encryptedBuffer
+    );
 
-  // Convert the decrypted buffer to a string
-  const decryptedText = new TextDecoder().decode(decryptedBuffer);
-
-  return decryptedText;
+    // Convert the decrypted buffer to a string
+    return new TextDecoder().decode(decryptedBuffer);
 }
